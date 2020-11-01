f2610ae6f77d092a208f899fd31d6baecb842320
JFigu <JFigu@DESKTOP-PNJ8QBL.fios-router.home>
2020-11-01 14:55:24 -0500

    i have created a new framework, i updates automation class t handle new locators. this is done to tell whoever looks at it what have you have pushed, and give a brief overview can read your message instead of looking through whole message.

---
+++ src/main/java/CBTest/CBTest/App.java
@@ -0,0 +1,13 @@
+package CBTest.CBTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
